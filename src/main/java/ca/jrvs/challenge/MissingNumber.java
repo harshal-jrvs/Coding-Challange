cdb63c93fb2416a6b635cdeca8195fcdfa9e174e
harshal-jrvs <harshal.bilimoria-jrvs.ca@gmail.com>
2019-07-13 23:15:57 +0000

    Commit

---
+++ src/main/java/ca/jrvs/challenge/MissingNumber.java
@@ -0,0 +1,24 @@
+package ca.jrvs.challenge;
+
+/**
+ * Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [3,0,1]
+ * Output: 2
+ * Example 2:
+ * <p>
+ * Input: [9,6,4,2,3,5,7,0,1]
+ * Output: 8
+ */
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int len = nums.length;
+        int sum = (0 + len) * (len + 1) / 2;
+        for (int i = 0; i < len; i++)
+            sum -= nums[i];
+        return sum;
+    }
+}
+
