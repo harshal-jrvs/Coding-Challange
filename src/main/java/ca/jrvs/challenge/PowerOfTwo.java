476864c507b0fc8a1238cfe756c7c64766291159
harshal-jrvs <harshal.bilimoria-jrvs.ca@gmail.com>
2019-07-20 18:33:18 +0000

    Commit

--- src/main/java/ca/jrvs/challenge/PowerOfTwo.java
+++ src/main/java/ca/jrvs/challenge/PowerOfTwo.java
@@ -1,5 +1,24 @@
 package ca.jrvs.challenge;
 
+/**
+ * /**
+ * Given an integer, write a function to determine if it is a power of two.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 1
+ * Output: true
+ * Explanation: 20 = 1
+ * Example 2:
+ * <p>
+ * Input: 16
+ * Output: true
+ * Explanation: 24 = 16
+ * Example 3:
+ * <p>
+ * Input: 218
+ * Output: false
+ **/
 public class PowerOfTwo {
 
     public boolean isPowerOfTwo(int n) {
