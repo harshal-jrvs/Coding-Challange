61179bede64111ae8ed5aeadc3fd6e10bcd109df
harshal-jrvs <harshal.bilimoria-jrvs.ca@gmail.com>
2019-07-21 21:22:32 +0000

    Commit

---
+++ src/main/java/ca/jrvs/challenge/ReverseLinkedList.java
@@ -0,0 +1,36 @@
+package ca.jrvs.challenge;
+
+/**
+ * Reverse a singly linked list.
+ * <p>
+ * Example:
+ * <p>
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ */
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
+
+
+
+
