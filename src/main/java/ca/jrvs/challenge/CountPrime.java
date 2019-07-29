da0c35c533d4a4694f8dd47eff986b2ebc6711b2
harshal-jrvs <harshal.bilimoria-jrvs.ca@gmail.com>
2019-07-29 18:33:55 +0000

    Commit

--- src/main/java/ca/jrvs/challenge/CountPrime.java
+++ src/main/java/ca/jrvs/challenge/CountPrime.java
@@ -1,5 +1,14 @@
 package ca.jrvs.challenge;
 
+/**
+ * Count the number of prime numbers less than a non-negative number, n.
+ * <p>
+ * Example:
+ * <p>
+ * Input: 10
+ * Output: 4
+ * Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+ */
 public class CountPrime {
     public int countPrimes(int n) {
         boolean isPrime = true;
--- src/main/java/ca/jrvs/challenge/ReverseString.java
+++ src/main/java/ca/jrvs/challenge/ReverseString.java
@@ -1,5 +1,32 @@
 package ca.jrvs.challenge;
 
+/**
+ * Given an input string, reverse the string word by word.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: "the sky is blue"
+ * Output: "blue is sky the"
+ * Example 2:
+ * <p>
+ * Input: "  hello world!  "
+ * Output: "world! hello"
+ * Explanation: Your reversed string should not contain leading or trailing spaces.
+ * Example 3:
+ * <p>
+ * Input: "a good   example"
+ * Output: "example good a"
+ * Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ * <p>
+ * <p>
+ * Note:
+ * <p>
+ * A word is defined as a sequence of non-space characters.
+ * Input string may contain leading or trailing spaces. However, your reversed string should not contain leading or trailing spaces.
+ * You need to reduce multiple spaces between two words to a single space in the reversed string.
+ */
 public class ReverseString {
     public String reverseWords(String s) {
         String[] trimmed = s.trim().split("\\s+");
